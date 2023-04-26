1222f7ca232d866ff4ecad6d13e4e73e455911a0
laivwxyz <delacruzlailanie2004@gmail.com>
2023-04-26 20:38:00 +0800

    Problem in this program has been added

--- multiple_line.py
+++ multiple_line.py
@@ -1,3 +1,11 @@
 # Dela Cruz, Lailanie E. _ BSCpE 1-4
 # Assignment 4
-# Programming Exercises 4
+# Programming Exercises 3
+
+# Write a method in python to write multiple line of text contents into a text file mylife.txt. See sample output:
+# Enter line: Beautiful is better than ugly.
+# Are there more lines y/n? y
+# Enter line: Explicit is better than implicit.
+# Are there more lines y/n? y
+# Enter line: Simple is better than complex.
+# Are there more lines y/n? n
